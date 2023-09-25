636c94b90a5d27782458a7c8439901aa377d8087
Eunjae Shim <53839819+eunjaeshim@users.noreply.github.com>
2023-09-25 11:35:47 -0400

    Further code organization

--- Dataloader.py
+++ Dataloader.py
@@ -3,42 +3,7 @@ import pandas as pd
 from copy import deepcopy
 from rdkit import Chem
 from rdkit.Chem import rdFingerprintGenerator
-
-
-DEOXY_SUBSTRATE_SMILES = [
-    "OCCCCC1=CC=CC=C1",
-    "OC(C)CCC1=CC=CC=C1",
-    "OC(C)(C)CCC1=CC=CC=C1",
-    "OCCCC1=NC(C2=CC=CC=C2)=C(C3=CC=CC=C3)O1",
-    "O=C(C(N(C)C=N1)=C1N2C)N(CCCCC(O)C)C2=O",
-    "O=C(C1=CC=CC=C1)N(C(C2=CC=CC=C2)=O)C3=NC=NC4=C3N=CN4[C@H]5[C@H](OC(C6=CC=CC=C6)=O)[C@H](OC(C7=CC=CC=C7)=O)[C@@H](CO)O5",
-    "ClC1=CC([N+]([O-])=O)=CC=C1/N=N/C2=CC=C(N(CC)CCO)C=C2",
-    "OCC1N(CCCC2=CC=CC=C2)CCCC1",
-    "O[C@@H]1C[C@H](OCC2=CC=CC=C2)C1",
-    "O=C(OC)[C@H]1N(C(OC(C)(C)C)=O)C[C@H](O)C1",
-    "O=C(OC)[C@H]1N(C(OC(C)(C)C)=O)C[C@@H](O)C1",
-    "CC1(C)OC[C@H]([C@@H]2[C@@H](O)[C@@H](OC(C)(C)O3)[C@@H]3O2)O1",
-    "O[C@@H]1CO[C@]2([H])[C@@H](OC(C)=O)CO[C@@]21[H]",
-    "O[C@@H]1CC[C@@H](N2C(C(C=CC=C3)=C3C2=O)=O)CC1",
-    "O[C@H]1CC[C@@H](N2C(C(C=CC=C3)=C3C2=O)=O)CC1",
-    "O[C@]1([H])C[C@H](CC2)N(C(OC(C)(C)C)=O)[C@H]2C1",
-    "OCC(C=C1)=CC=C1C2=CC=CC=C2",
-    "OCC1=CC=C(S(N(CCC)CCC)(=O)=O)C=C1",
-    "ClC(N=C1CCCC)=C(CO)N1CC(C=C2)=CC=C2C3=C(C4=NN=NN4C(C5=CC=CC=C5)(C6=CC=CC=C6)C7=CC=CC=C7)C=CC=C3",
-    "OCN1C(C)=CC(C)=N1",
-    "COC1=C(OCCCC(OC)=O)C=C([N+]([O-])=O)C(C(O)C)=C1",
-    "O[C@@H](C1=CC=CC=C1)[C@H](C)N2CCCC2",
-    "C/C(C)=C/CC/C(C)=C/CC/C(C)=C/CO",
-    "CCCCCCCC(O)C=C",
-    "C/C(C)=C/CCC(O)(C=C)C",
-    "O=C(C1=CC=C(Cl)C=C1)N2C3=CC=C(OC)C=C3C(CCO)=C2C",
-    "CC1=NC=C([N+]([O-])=O)N1CCO",
-    "O[C@@H](C1)CC[C@@]2(C)C1=CC[C@]3([H])[C@]2([H])CC[C@@]4(C)[C@@]3([H])CC=C4C5=CN=CC=C5",
-    "O=C1C(O)CCO1",
-    "O=C1C=C[C@@]2(C)C(CC[C@]([C@@](CC[C@@]3(C(CO)=O)O)([H])[C@]3(C)C4)([H])[C@]2([H])C4=O)=C1",
-    "OC[C@@H](C(OC)=O)NC(C1=CC=CC=C1)(C2=CC=CC=C2)C3=CC=CC=C3",
-    "C[C@@]12CC[C@]3([H])[C@]4(OO2)[C@@](O[C@H](O)[C@H](C)C4CC[C@H]3C)([H])O1",
-]
+from abc import ABC, abstractmethod 
 
 
 def yield_to_ranking(yield_array):
@@ -66,6 +31,26 @@ def yield_to_ranking(yield_array):
         raw_rank = len(yield_array) - np.argsort(np.argsort(yield_array))
         raw_rank[np.where(raw_rank == 0)[0]] = len(raw_rank)
     return raw_rank
+
+
+class Dataset(ABC):
+    """
+    Base class for preparing datasets.
+    
+    Parameters
+    ----------
+    for_regressor : bool
+        Whether the input will be used for training a regressor.
+    n_rxns : int
+        Number of reactions that we simulate to select and conduct.
+    """
+    def __init__(
+        self,
+        for_regressor,
+        n_rxns
+    ) :
+        self.for_regressor = for_regressor
+        self.n_rxns = n_rxns
 
 
 class DeoxyDataset :
@@ -100,7 +85,10 @@ class DeoxyDataset :
         self.train_together = train_together
         self.n_rxns = n_rxns
 
-        self.substrate_smiles = DEOXY_SUBSTRATE_SMILES
+        self.substrate_smiles = list(pd.read_excel(
+            "datasets/deoxyfluorination/substrate_smiles.xlsx",
+            header=None
+        ))
         self.descriptors = pd.read_csv(
             "datasets/deoxyfluorination/descriptor_table.csv"
         ).to_numpy()
@@ -112,8 +100,33 @@ class DeoxyDataset :
             .to_numpy()
             .flatten()
         )
+        if self.component_to_rank == "sulfonyl_fluoride" :
+            self.n_non_rank_component = 4 # 4 bases
+        elif self.component_to_rank == "base":
+            self.n_non_rank_component = 5 # 5 sulfonyl fluorides
     
     def _combine_desc_arrays(self, substrate_array, reagent_array, n_base_bits=1):
+        """
+        Combines feature arrays of a substrate and a reagent that first alters through
+        the reagent, followed by substrate.
+        e.g. if there are two reagents:
+          row1=(substrate1 + reagent1) // row2=(substrate1 + reagent2)
+          row3=(substrate2 + reagent1) // row4=(substrate2 + reagent2)
+        
+        Parameters
+        ---------
+        substrate_array : np.ndarray of shape (n_substrates, n_features)
+            Feature array of substrates
+        reagent_array : np.ndarray of shape (n_reagents, n_features)
+            Feature array of reagents
+        n_base_bits : int
+            Number of descriptor elements that bases take.
+        
+        Returns
+        -------
+        combined_array : np.ndarray of shape (n_substrates * n_reagents, n_features_total)
+            Combined feature array.
+        """
         if self.component_to_rank == "sulfonyl_fluoride" :
             combined_array = np.hstack((
                 np.repeat(substrate_array, 4, axis=0),
@@ -176,11 +189,9 @@ class DeoxyDataset :
             elif self.component_to_rank in ["sulfonyl_fluoride", "base"]:
                 if self.train_together :
                     self._X_fp = self._combine_desc_arrays(fp_array, reagent_desc)
-                if not self.train_together :
-                    if self.component_to_rank == "sulfonyl_fluoride" :
-                        self._X_fp = [fp_array]*4
-                    else :
-                        self._X_fp = [fp_array]*5
+                else :
+                    self._X_fp = [fp_array]*self.n_non_rank_component
+                    
         return self._X_fp
     
     
@@ -196,15 +207,15 @@ class DeoxyDataset :
                 self._X_desc = self.descriptors[[20 * x for x in range(32)], :19] #  19=number of substrate descriptors
             elif self.component_to_rank in ["base", "sulfonyl_fluoride"] :
                 if self.train_together :
+                    # This case the features of reaction component that is not ranked are inputs
                     self._X_desc = self._combine_desc_arrays(
                         self.descriptors[[20 * x for x in range(32)], :19],
                         self.descriptors[[20 * x for x in range(32)], 19:]
                     )
                 else :
-                    if self.component_to_rank == "sulfonyl_fluoride" :
-                        self._X_desc = [self.descriptors[[20 * x for x in range(32)], :19]]*4
-                    else :
-                        self._X_desc = [self.descriptors[[20 * x for x in range(32)], :19]]*5
+                    # This case the substrates are the only inputs.
+                    self._X_desc = [self.descriptors[[20 * x for x in range(32)], :19]]*self.n_non_rank_component
+                    
         return self._X_desc
 
     @property
@@ -223,22 +234,41 @@ class DeoxyDataset :
                         4
                     )
                 else :
-                    if self.component_to_rank == "sulfonyl_fluoride" :
-                        self._X_onehot = [self.onehot[[20 * x for x in range(32)], :32]]*4
-                    else :
-                        self._X_onehot = [self.onehot[[20 * x for x in range(32)], :32]]*5
+                    self._X_onehot = [self.onehot[[20 * x for x in range(32)], :32]]*self.n_non_rank_component
+
         return self._X_onehot
     
 
     def _split_yields(self, y_array, n_rows):
+        """ If the yield array is a repetition of yields from reactions of a set of reagents,
+        it splits the array such that each array is yields from only that reagent.
+        
+        Parameters
+        ----------
+        y_array : np.ndarray of shape (n_substrates*n_rxn_component1, n_rxn_component2)
+            Yield array of reactions where rows are repeated by some reaction component.    
+            i.e. row1 = substrate 1 & reagent A, row2 = substrate 1 & reagent B, 
+                 row3 = substrate 2 & reagent A, row4 = substrate 2 & reagent B
+                 •••
+                 with each row having lengths of number of reaction component 2
+        n_rows : int
+            Number of different reagents used for reaction component 1 as in the description above.
+            
+        Returns
+        -------
+        y_by_reagent : list of n_rxn_component1 number of np.ndarray of shape (n_substrates, n_rxn_component2)
+            Yield arrays for each reaction component1.
+        """
         y_by_reagent = [[]] * n_rows
         for ind, row in enumerate(y_array) :
             where = ind % n_rows
             y_by_reagent[where].append(row)
+        y_by_reagent = [np.vstack(tuple(x)) for x in y_by_reagent]
         return y_by_reagent
 
     @property
     def y_yield(self):
+        """ Prepares continuous yield value arrays appropriate for each problem setting. """
         if self.for_regressor : 
             self._y_yield = deepcopy(self.yields)
         else :
@@ -248,34 +278,41 @@ class DeoxyDataset :
                 if self.train_together :
                     self._y_yield = self.yields.reshape(32 * 4, 5)
                 else :
-                    yield_by_reagent = self._split_yields(self.yields.reshape(32 * 4, 5), 4)
-                    self._y_yield = [np.vstack(tuple(x)) for x in yield_by_reagent]
+                    self._y_yield = self._split_yields(self.yields.reshape(32 * 4, 5), 4)
             elif self.component_to_rank == "base":
                 yield_together = np.vstack(tuple([row.reshape(4,5).T for row in self.yields.reshape(32,20)]))
                 if self.train_together :
                     self._y_yield = deepcopy(yield_together)
                 else : 
-                    yield_by_reagent = self._split_yields(yield_together, 5)
-                    self._y_yield = [np.vstack(tuple(x)) for x in yield_by_reagent]
+                    self._y_yield = self._split_yields(yield_together, 5)
 
         return self._y_yield
 
     @property
     def y_ranking(self):
+        """ Transforms raw continuous yield values into arrays of ranks. """
         if type(self._y_yield) == list :
             self._y_ranking = [yield_to_ranking(x) for x in self._y_yield]
         else :
             self._y_ranking = yield_to_ranking(self._y_yield)
-        # if self.component_to_rank == "both":
-        #     self._y_ranking = yield_to_ranking(self.yields.reshape(32, 20))
-        # elif self.component_to_rank == "sulfony_fluoride" :
-        #     self._y_ranking = yield_to_ranking(self.yields.reshape(32 * 4, 5))
-        # elif self.component_to_rank == "base":
-        #     self._y_ranking = yield_to_ranking(self.yields.reshape(32 * 5, 4))
         return self._y_ranking
     
 
-    def _transform_to_multilabel(self, yield_array) :
+    def _transform_yield_to_multilabel(self, yield_array) :
+        """
+        For each substrate (pair), the array of yields under multiple reaction conditions
+        are transformed into a multilabel array, marking top-k reaction conditions as positives.
+        
+        Parameters
+        ----------
+        yield_array : np.ndarray of shape (n_substrates, n_reaction_conditions)
+            Array of continuous yield values.
+        
+        Returns
+        -------
+        multilabel_array : np.ndarray of shape (n_substrates, n_reaction_conditions)
+            Array to be used for multilabel classification.
+        """
         positive_inds = list(np.argpartition(-1*yield_array, self.n_rxns, axis=1)[:self.n_rxns])
         multilabel_array = np.zeros_like(yield_array)
         for i, row in enumerate(yield_array):
@@ -287,12 +324,14 @@ class DeoxyDataset :
     def y_label(self):
         """ 
         Prepares output array to be used for conventional classifications.
+        If we will select one highest prediction, we formulate a multiclass classification.
+        If we select more than one, a multilabel classification is formulated.
         """
         if self.n_rxns > 1:
             if self.train_together :
-                self._y_label = self._transform_to_multilabel(self._y_yield)
+                self._y_label = self._transform_yield_to_multilabel(self._y_yield)
             else : 
-                self._y_label = [self._transform_to_multilabel(x) for x in self._y_yield]
+                self._y_label = [self._transform_yield_to_multilabel(x) for x in self._y_yield]
         elif self.n_rxns == 1 :
             if type(self._y_yield) == list:
                 self._y_label = [np.argmin(x, axis=1) for x in np.argmin(self._y_yield, axis=1)]
--- Evaluator.py
+++ Evaluator.py
@@ -1,6 +1,7 @@
 from scipy.stats import kendalltau
 import numpy as np
 from Dataloader import yield_to_ranking
+from abc import ABC, abstractmethod
 
 PERFORMANCE_DICT = {
     "kendall_tau": [],
@@ -12,87 +13,139 @@ PERFORMANCE_DICT = {
 }
 
 
-def update_perf_dict(perf_dict, kt, rr, mrr, regret, comp, model):
-    """ Updates the dictionary keeping track of performances.
+class Evaluator(ABC) :
+    """ Base class for evaluators for various types of algorithsm.
     
     Parameters
     ----------
-    perf_dict : dictionary
-        Has 6 keys as  you can see below.
-    kt : list or float
-        Kendall-tau ranking correlation scores.
-        This being a list comes from the case when multiple rows are evaluated simultaneously.
-    rr : list or float
-        Reciprocal (ground-truth) rank of the best selection by the model.
-    mrr : list or float
-        Mean reciprocal (ground-truth) rank of all selections by the model.
-    regret : list or float
-        Ground-truth best yield - ground-truth yield of best selection by model.
-    comp : int
-        Compound index of the test compound.
-    model : str
-        Name of the model
-    
-    Returns
-    -------
-    None
-        The perf_dict input is updated in-place.
+    dataset : Dataset object from Dataloader.py
+        Dataset to utilize.
+    n_rxns : int
+        Number of reactions that is simulated to be selected and conducted.
+    outer_cv : sklearn split object
+        Cross-validation scheme to 'evaluate' the algorithms.
     """
-    if type(kt) != list:
-        perf_dict["kendall_tau"].append(kt)
-        perf_dict["reciprocal_rank"].append(rr)
-        perf_dict["mean_reciprocal_rank"].append(mrr)
-        perf_dict["regret"].append(regret)
-        perf_dict["test_compound"].append(comp)
-        perf_dict["model"].append(model)
-    elif type(kt) == list:
-        assert len(kt) == len(rr)
-        assert len(rr) == len(mrr)
-        perf_dict["kendall_tau"].extend(kt)
-        perf_dict["reciprocal_rank"].extend(rr)
-        perf_dict["mean_reciprocal_rank"].extend(mrr)
-        perf_dict["regret"].extend(regret)
-        if type(comp) == list:
-            perf_dict["test_compound"].extend(comp)
-        elif type(comp) == int:
-            perf_dict["test_compound"].extend([comp] * len(kt))
-        if type(model) == "list":
-            perf_dict["model"].extend(model)
-        elif type(model) == str:
-            perf_dict["model"].extend([model] * len(kt))
+    def __init__(
+        self,
+        dataset,
+        n_rxns,
+        outer_cv
+    ):
+        self.dataset = dataset
+        self.n_rxns = n_rxns
+        self.outer_cv = outer_cv
+        self.perf_dict = PERFORMANCE_DICT
 
 
-def evaluate_lr_alg(test_yield, test_rank, pred_rank, n_rxns, perf_dict, comp, model):
-    if np.ndim(pred_rank) == 1 :
-        kt = kendalltau(test_rank, pred_rank).statistic
-        predicted_highest_yield_inds = np.argpartition(pred_rank.flatten(), n_rxns)[:n_rxns]
-        best_retrieved_yield = np.max(test_yield[predicted_highest_yield_inds])
-        actual_inds_with_that_yield = np.where(test_yield == best_retrieved_yield)[0]
-        rr = 1 / np.min(test_rank[actual_inds_with_that_yield])
-        mrr = np.mean(
-            np.reciprocal(test_rank[predicted_highest_yield_inds], dtype=np.float32)
-        )
-        regret = max(test_yield) - max(test_yield[predicted_highest_yield_inds])
-    elif np.ndim(pred_rank) == 2 :
-        kt = [
-            kendalltau(test_rank[i, :], pred_rank[i, :]).statistic
-            for i in range(pred_rank.shape[0])
-        ]
-        predicted_highest_yield_inds = np.argpartition(pred_rank, n_rxns, axis=1)[:, :n_rxns]
-        best_retrieved_yield = [np.max(test_yield[i, row]) for i, row in enumerate(predicted_highest_yield_inds)]
-        actual_inds_with_that_yield = [np.where(test_yield == best_y)[0] for best_y in best_retrieved_yield]
-        rr = [
-            1 / np.min(test_rank[a, x]) for a, x in enumerate(actual_inds_with_that_yield)
-        ]
-        mrr = [
-            np.mean(np.reciprocal(test_rank[a, row])) for a, row in enumerate(predicted_highest_yield_inds)
-        ]
-        raw_regret = np.max(test_yield, axis=1) - np.max(np.vstack(tuple([test_yield[i, row] for i, row in enumerate(predicted_highest_yield_inds)])), axis=1)
-        regret = list(raw_regret.flatten())
-    update_perf_dict(perf_dict, kt, rr, mrr, regret, comp, model)
+    def _update_perf_dict(perf_dict, kt, rr, mrr, regret, comp, model):
+        """ Updates the dictionary keeping track of performances.
+        
+        Parameters
+        ----------
+        perf_dict : dictionary
+            Has 6 keys as  you can see below.
+        kt : list or float
+            Kendall-tau ranking correlation scores.
+            This being a list comes from the case when multiple rows are evaluated simultaneously.
+        rr : list or float
+            Reciprocal (ground-truth) rank of the best selection by the model.
+        mrr : list or float
+            Mean reciprocal (ground-truth) rank of all selections by the model.
+        regret : list or float
+            Ground-truth best yield - ground-truth yield of best selection by model.
+        comp : int
+            Compound index of the test compound.
+        model : str
+            Name of the model
+        
+        Returns
+        -------
+        None
+            The perf_dict input is updated in-place.
+        """
+        if type(kt) != list:
+            perf_dict["kendall_tau"].append(kt)
+            perf_dict["reciprocal_rank"].append(rr)
+            perf_dict["mean_reciprocal_rank"].append(mrr)
+            perf_dict["regret"].append(regret)
+            perf_dict["test_compound"].append(comp)
+            perf_dict["model"].append(model)
+        elif type(kt) == list:
+            assert len(kt) == len(rr)
+            assert len(rr) == len(mrr)
+            perf_dict["kendall_tau"].extend(kt)
+            perf_dict["reciprocal_rank"].extend(rr)
+            perf_dict["mean_reciprocal_rank"].extend(mrr)
+            perf_dict["regret"].extend(regret)
+            if type(comp) == list:
+                perf_dict["test_compound"].extend(comp)
+            elif type(comp) == int:
+                perf_dict["test_compound"].extend([comp] * len(kt))
+            if type(model) == "list":
+                perf_dict["model"].extend(model)
+            elif type(model) == str:
+                perf_dict["model"].extend([model] * len(kt))
 
 
-class BaselineEvaluator:
+    def _evaluate_alg(self, test_yield, pred_rank, comp, model):
+        """ Evaluates algorithms on the following metrics 
+        • 'reciprocal rank' : of the selected n_rxns, the reciprocal of the best ground-truth-rank.
+        • 'mean reciprocal rank' : mean of the reciprocal of ground-truth-ranks of all rxns selected by model.
+        • 'regret' : the numerical difference between the ground-truth highest yield and
+                     the best yield retrieved by model.
+        and updates the performance dictionary.
+
+        Parameters
+        ----------
+        test_yield : np.ndarray of shape (n_test_rxns, ) or (n_test_substrates, n_rxn_conditions)
+            Array of numerical yields from the test set.
+        pred_rank : np.ndarray of shape (n_test_substrates, n_rxn_conditions)
+            Array of predicted ranks under each reaction condition
+        comp : int
+            Index of test compound.
+        model : str
+            Name of the algorithm that is evaluated.
+        
+        Returns
+        -------
+        self (with updated perf_dict)
+        """
+        test_rank = yield_to_ranking(test_yield)
+        if np.ndim(pred_rank) == 1 :
+            kt = kendalltau(test_rank, pred_rank).statistic
+            predicted_highest_yield_inds = np.argpartition(pred_rank.flatten(), self.n_rxns)[:self.n_rxns]
+            best_retrieved_yield = np.max(test_yield[predicted_highest_yield_inds])
+            actual_inds_with_that_yield = np.where(test_yield == best_retrieved_yield)[0]
+            rr = 1 / np.min(test_rank[actual_inds_with_that_yield])
+            mrr = np.mean(
+                np.reciprocal(test_rank[predicted_highest_yield_inds], dtype=np.float32)
+            )
+            regret = max(test_yield) - max(test_yield[predicted_highest_yield_inds])
+
+        elif np.ndim(pred_rank) == 2 :
+            kt = [
+                kendalltau(test_rank[i, :], pred_rank[i, :]).statistic
+                for i in range(pred_rank.shape[0])
+            ]
+            predicted_highest_yield_inds = np.argpartition(pred_rank, self.n_rxns, axis=1)[:, :self.n_rxns]
+            best_retrieved_yield = [np.max(test_yield[i, row]) for i, row in enumerate(predicted_highest_yield_inds)]
+            actual_inds_with_that_yield = [np.where(test_yield == best_y)[0] for best_y in best_retrieved_yield]
+            rr = [
+                1 / np.min(test_rank[a, x]) for a, x in enumerate(actual_inds_with_that_yield)
+            ]
+            mrr = [
+                np.mean(np.reciprocal(test_rank[a, row])) for a, row in enumerate(predicted_highest_yield_inds)
+            ]
+            raw_regret = np.max(test_yield, axis=1) - np.max(np.vstack(tuple([test_yield[i, row] for i, row in enumerate(predicted_highest_yield_inds)])), axis=1)
+            regret = list(raw_regret.flatten())
+        self._update_perf_dict(self.perf_dict, kt, rr, mrr, regret, comp, model)
+
+    @abstractmethod
+    def train_and_evaluate_models(self) :
+        pass
+
+
+class BaselineEvaluator(Evaluator):
     """ Evaluates the baseline model of selecting based on average yield in the training dataset.
     
     Parameters
@@ -108,12 +161,9 @@ class BaselineEvaluator:
         n_rxns,
         outer_cv
     ) :
-        self.dataset = dataset
-        self.n_rxns = n_rxns
-        self.outer_cv = outer_cv
+        super().__init__(dataset, n_rxns, outer_cv)
 
     def train_and_evaluate_models(self):  
-        self.perf_dict = PERFORMANCE_DICT  
         y = self.dataset.y_yield
         for i, (train_ind, test_ind) in enumerate(self.outer_cv.split(())) :
             if type(y) == list :
@@ -123,19 +173,16 @@ class BaselineEvaluator:
 
             else :
                 y_train, y_test = y[train_ind], y[test_ind]
-                evaluate_lr_alg(
+                self._evaluate_alg(
                     y_test,
-                    yield_to_ranking(y_test),
                     np.tile(yield_to_ranking(np.mean(y_train, axis=0)), (y_test.shape[0], 1)),
-                    self.n_rxns,
-                    self.perf_dict,
                     i,
                     "baseline",
                 )
         return self
 
 
-class RegressorEvaluator:
+class RegressorEvaluator(Evaluator):
     """ Evaluates regressors for a specific dataset.
     
     Parameters
@@ -160,14 +207,12 @@ class RegressorEvaluator:
         list_of_names,
         outer_cv
     ) :
-        self.regressor_dataset = regressor_dataset
+        super().__init__(regressor_dataset, n_rxns, outer_cv)
         self.feature_type = feature_type
-        self.n_rxns = n_rxns
         self.list_of_algorithms = list_of_algorithms
         self.list_of_names = list_of_names
-        self.outer_cv = outer_cv
 
-    
+
     def train_and_evaluate_models(self):
         """ 
         Trains models.
@@ -187,14 +232,6 @@ class RegressorEvaluator:
 
         self.models = [[] for _ in range(len(self.list_of_algorithms))]
         self.cv_scores = [[] for _ in range(len(self.list_of_algorithms))]
-        self.perf_dict = {
-            "kendall_tau": [],
-            "reciprocal_rank": [],
-            "mean_reciprocal_rank": [],
-            "regret":[],
-            "test_compound": [],
-            "model": [],
-        }
 
         for i, (train_ind, test_ind) in enumerate(self.outer_cv.split(())) :
             print(f"Evaluating on compound {i}")
@@ -209,12 +246,9 @@ class RegressorEvaluator:
                 self.cv_scores[j].append(model.best_score_)
                 y_pred = best_model.predict(X_test)
 
-                evaluate_lr_alg(
+                self._evaluate_alg(
                     y_test, 
-                    yield_to_ranking(y_test), 
                     yield_to_ranking(y_pred), 
-                    self.n_rxns, 
-                    self.perf_dict, 
                     i, 
                     model_name
                 )
